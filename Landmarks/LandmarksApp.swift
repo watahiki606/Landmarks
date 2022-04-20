de556e32d04b0e71e03e68bd1635ef2b9d6c49d5
watahiki606 <v10acdict@gmail.com>
2022-04-20 13:24:19 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 綿引慎也 on 2022/04/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
